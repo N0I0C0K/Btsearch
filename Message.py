a4ff848ebe2ff27a6c017c1e21b9895a05f185f8
N0I0C0K <nick131410@aliyun.com>
2019-08-09 20:59:32 +0800

    update new function

--- Message.py
+++ Message.py
@@ -50,8 +50,7 @@ class Messager():
             #self.__mesagelist[index]['isUsed'] = True
             return resultmessageList
         else:
-            return False
-    
+            return False 
     def getAMessageByTargetName(self,target):
         if not target in self.__mesageheadlist:
             return False
@@ -67,7 +66,39 @@ class Messager():
             return self.__mesagelist[index]
         else:
             return False
-        return
+    def readAllMessagebyTargetName(self,target):
+        if not target in self.__mesageheadlist:
+            return False
+        if target in self.__mesageheadlist[self.tail_:self.head_]:
+            indexlist = []
+            index = self.tail_-1
+            while index+1 < self.head_:
+                if not target in self.__mesageheadlist[index+1:self.head_]:
+                    break
+                index = self.__mesageheadlist.index(target,index+1,self.head_)
+                if self.__mesagelist[index]['isUsed'] == True:
+                    continue
+                indexlist.insert(-1,index)
+            resultmessageList = []
+            for i in indexlist:
+                resultmessageList.insert(-1,self.__mesagelist[i])        
+            return resultmessageList
+        else:
+            return False
+        def readAMessageByTargetName(self,target):
+            if not target in self.__mesageheadlist:
+                return False
+            if target in self.__mesageheadlist[self.tail_:self.head_]:
+                index = self.tail_ - 1
+                while index+1 < self.head_:
+                    if not target in self.__mesageheadlist[index+1:self.head_]:
+                        break
+                    index = self.__mesageheadlist.index(target,index+1,self.head_)
+                    if self.__mesagelist[index]['isUsed'] == False:
+                        break   
+                return self.__mesagelist[index]
+            else:
+                return False 
     def __popMessage(self,index):
         
         return
